b66457b18a998a4235f80dd668a902ab0c88612a
Joe Smith <yasumoto7@gmail.com>
2021-04-16 21:34:23 -0700

    Proper font

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -2,10 +2,21 @@
 
 # Setup a development Ubuntu box (should also work with WSL)
 
+set -eux
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
 sudo apt-get install fish jq fortune-mod nmap nghttp2-client shellcheck pipenv powerline
 
 chsh -s /usr/bin/fish
 
+./oh-my-fish/bin/install --offline
+
 ./_bootstrap_homedir_config_files.sh
 
-./oh-my-fish/bin/install --offline
+if uname -r | grep -qi wsl; then
+    echo "Make sure you download the powerline-compatible font at:"
+    echo "https://github.com/microsoft/cascadia-code/releases/tag/latest"
+fi
