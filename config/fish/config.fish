60d904cf503852622966ce83109491b500b760da
Joe Smith <yasumoto7@gmail.com>
2021-05-29 12:55:17 -0700

    Locally built tools on PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,11 @@
 function fish_greeting
+    echo
     fortune
+    echo
 end
 
 set fish_function_path $fish_function_path "/usr/local/lib/python3.9/site-packages/powerline/bindings/fish"
 set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
 powerline-setup
 
-set PATH $PATH "$HOME/.local/bin"
+set PATH $PATH "$HOME/.local/bin" "$HOME/workspace/go/bin" "$HOME/workspace/bin"
