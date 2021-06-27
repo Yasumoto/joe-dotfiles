fb0b05a6dfea1f550cb959257f5ff6a149f0cf06
Joe Smith <yasumoto7@gmail.com>
2021-06-27 11:26:43 +0000

    Use SASL instead of nickserv

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -67,7 +67,7 @@ if command -v dconf > /dev/null; then
 fi
 
 echo "🗣️ Setting irssi config, "
-if command -v snap > /dev/null; then
+if [ "$(which irssi)" = "" ] && command -v snap > /dev/null; then
     snap install irssi
     IRSSI_CONFIG_PATH="${HOME}/snap/irssi/common/.irssi/config"
 else
