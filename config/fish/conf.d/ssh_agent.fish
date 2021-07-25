f2027275d305be3eb49ff8acab22bf6d5b2517ef
Joe Smith <yasumoto7@gmail.com>
2021-07-25 23:32:44 +0000

    Port over the SSH agent setup to fish

---
+++ config/fish/conf.d/ssh_agent.fish
@@ -0,0 +1,27 @@
+set -e SSH_AUTH_SOCK
+
+for PATHNAME in /tmp/ssh-*/agent.*
+    # If this isn't writable by us, get outta here
+    if [ ! -w $PATHNAME ];
+        continue
+    end
+
+    if [ (SSH_AUTH_SOCK=$PATHNAME ssh-add -l 2>&1) != "Could not open a connection to your authentication agent" ];
+        set -gx SSH_AUTH_SOCK $PATHNAME
+    else
+        rm -f $PATHNAME
+        rmdir (dirname $PATHNAME) 2> /dev/null || : # If that didn't work, that's fine
+    end
+end
+
+# Wasn't able to restore an existing agent
+if [ -z $SSH_AUTH_SOCK ]
+    eval (ssh-agent -c)
+    ssh-add
+end
+
+if [ $SSH_AUTH_SOCK != "$HOME/.ssh/agent" ]
+    ln -fs $SSH_AUTH_SOCK "$HOME/.ssh/agent"
+end
+
+set -gx SSH_AUTH_SOCK "$HOME/.ssh/agent"
