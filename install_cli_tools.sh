e498d23bda7dfa8f8ce926b626bd58553c9bbe0f
Joe Smith <yasumoto7@gmail.com>
2021-07-30 08:51:07 -0700

    difftool and gh

--- install_cli_tools.sh
+++ install_cli_tools.sh
@@ -97,5 +97,12 @@ if [ "$(which docker)" = "" ]; then
     sudo apt-get install docker-ce docker-ce-cli containerd.io
 fi
 
+if [ "$(which gh)" = "" ]; then
+    echo "🐙️ Installing gh"
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt update
+    sudo apt install gh
+fi
 
 echo "🐧️ Happy hacking!"
