4f9b323208247e3c7520414496494b363344a1ce
Joe Smith <yasumoto7@gmail.com>
2021-08-12 20:26:49 -0700

    Fixing clone function

--- config/fish/functions/clone.fish
+++ config/fish/functions/clone.fish
@@ -7,10 +7,10 @@ function clone
     else
         set PROVIDER (echo $LOCATION | cut -f1 -d: | cut -f2 -d\@)
         set OWNER (echo $LOCATION | cut -f2 -d: | cut -f1 -d/)
-        set REPO (echo $LOCATION | cut -f2 -d: | cut -f3 -d/ | cut -f1 -d.)
+        set REPO (echo $LOCATION | cut -f2 -d: | cut -f2 -d/ | cut -f1 -d.)
 	if [ -n $REPO ]
           # This is a GitLab-ism
-          set SUBPROJECT (echo $LOCATION | cut -f2 -d: | cut -f2 -d/ | cut -f1 -d.)
+          set SUBPROJECT (echo $LOCATION | cut -f2 -d: | cut -f3 -d/ | cut -f1 -d.)
 	end
     end
 
@@ -19,6 +19,13 @@ function clone
     else
       set FILESYSTEM_LOCATION "/home/$USER/workspace/$PROVIDER/$OWNER"
     end
+
+    #echo $PROVIDER
+    #echo $OWNER
+    #echo $REPO
+    #echo $SUBPROJECT
+    #echo $FILESYSTEM_LOCATION
+    #return
 
     mkdir -p "$FILESYSTEM_LOCATION"
     git clone "$LOCATION" "$FILESYSTEM_LOCATION/$REPO"
