09175278cf81f8c00f112f762137a7b33d9e119c
Joe Smith <yasumoto7@gmail.com>
2021-08-13 01:58:30 -0700

    bat and delta for git diff

--- install_cli_tools.sh
+++ install_cli_tools.sh
@@ -14,12 +14,15 @@ TERRAFORM_TFLINT_VERSION="0.30.0"
 
 K9S_VERSION="0.24.13"
 
+BAT_VERSION="v0.18.2"
+DELTA_VERSION="0.8.3"
+
 mkdir -p ~/workspace/bin
 cd "${HOME}" || exit
 
 if [ "$(which aws)"  = "" ]; then
     echo "☁️ Installing aws-cli"
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${AWS_CLI_VERSION}.zip" -o "awscliv2.zip"
+    curl -L "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${AWS_CLI_VERSION}.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     ./aws/install --install-dir "${HOME}"/workspace/aws-cli --bin-dir "${HOME}"/workspace/bin
     rm ./awscliv2.zip
@@ -28,7 +31,7 @@ fi
 
 if [ "$(which eksctl)" = "" ]; then
     echo "🎛️ Installing eksctl"
-    curl -LO "https://github.com/weaveworks/eksctl/releases/download/${AWS_EKSCTL_VERSION}/eksctl_Linux_amd64.tar.gz"
+    curl -L -O "https://github.com/weaveworks/eksctl/releases/download/${AWS_EKSCTL_VERSION}/eksctl_Linux_amd64.tar.gz"
     tar -xzvf ./eksctl_Linux_amd64.tar.gz eksctl
     mv ./eksctl "${HOME}/workspace/bin"
     rm -f ./eksctl_Linux_amd64.tar.gz
@@ -36,7 +39,7 @@ fi
 
 if [ "$(which terraform)" = "" ]; then
     echo "🏗️ Installing terraform"
-    curl -O "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_ZIPFILE}"
+    curl -L -O "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_ZIPFILE}"
     unzip "${TERRAFORM_ZIPFILE}"
     mv ./terraform "${HOME}/workspace/bin"
     rm "${TERRAFORM_ZIPFILE}"
@@ -44,7 +47,7 @@ fi
 
 if [ "$(which terraform-docs)" = "" ]; then
     echo "📖️ Installing terraform-docs"
-    curl -OL "https://github.com/terraform-docs/terraform-docs/releases/download/v${TERRAFORM_DOCS_VERSION}/terraform-docs-v${TERRAFORM_DOCS_VERSION}-linux-amd64.tar.gz"
+    curl -L -O "https://github.com/terraform-docs/terraform-docs/releases/download/v${TERRAFORM_DOCS_VERSION}/terraform-docs-v${TERRAFORM_DOCS_VERSION}-linux-amd64.tar.gz"
     tar -xzvf "./terraform-docs-v${TERRAFORM_DOCS_VERSION}-linux-amd64.tar.gz" terraform-docs
     mv ./terraform-docs "${HOME}/workspace/bin"
     rm -f "./terraform-docs-v${TERRAFORM_DOCS_VERSION}-linux-amd64.tar.gz"
@@ -52,13 +55,13 @@ fi
 
 if [ "$(which tfsec)" = "" ]; then
     echo "🔒️ Installing tfsec"
-    curl -OL "https://github.com/tfsec/tfsec/releases/download/v${TERRAFORM_TFSEC_VERSION}/tfsec-linux-amd64"
+    curl -L -O "https://github.com/tfsec/tfsec/releases/download/v${TERRAFORM_TFSEC_VERSION}/tfsec-linux-amd64"
     mv ./tfsec-linux-amd64 "${HOME}/workspace/bin/tfsec"
     chmod +x "${HOME}/workspace/bin/tfsec"
 fi
 
 if [ "$(which tflint)" = "" ]; then
-    curl -OL "https://github.com/terraform-linters/tflint/releases/download/v${TERRAFORM_TFLINT_VERSION}/tflint_linux_amd64.zip"
+    curl -L -O "https://github.com/terraform-linters/tflint/releases/download/v${TERRAFORM_TFLINT_VERSION}/tflint_linux_amd64.zip"
     unzip ./tflint_linux_amd64.zip tflint
     mv ./tflint "${HOME}/workspace/bin"
     rm ./tflint_linux_amd64.zip
@@ -66,7 +69,7 @@ fi
 
 if [ "$(which kubectl)" = "" ]; then
     echo "☸️ Installing kubectl"
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -L -O "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x ./kubectl
     mv ./kubectl "${HOME}/workspace/bin"
     kubectl version --client
@@ -74,7 +77,7 @@ fi
 
 if [ "$(which k9s)" = "" ]; then
     echo "🐶️ Installing k9s"
-    curl -LO "https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_Linux_x86_64.tar.gz"
+    curl -L -O "https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_Linux_x86_64.tar.gz"
     tar -xzvf ./k9s_Linux_x86_64.tar.gz k9s
     rm ./k9s_Linux_x86_64.tar.gz
     mv ./k9s "${HOME}/workspace/bin"
@@ -103,6 +106,24 @@ if [ "$(which gh)" = "" ]; then
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     sudo apt update
     sudo apt install gh
+fi
+
+if [ "$(which bat)" = "" ]; then
+    echo "🦇️ Installing bat"
+    curl -L -O "https://github.com/sharkdp/bat/releases/download/${BAT_VERSION}/bat-${BAT_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+    tar -xzvf ./bat-${BAT_VERSION}-x86_64-unknown-linux-gnu.tar.gz bat-${BAT_VERSION}-x86_64-unknown-linux-gnu/bat
+    rm ./bat-${BAT_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+    mv ./bat-${BAT_VERSION}-x86_64-unknown-linux-gnu/bat "${HOME}/workspace/bin"
+    rm -rf ./bat-${BAT_VERSION}-x86_64-unknown-linux-gnu
+fi
+
+if [ "$(which delta)" = "" ]; then
+    echo "🌊️ Installing delta"
+    curl -L -O "https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+    tar -xzvf ./delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu.tar.gz delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu/delta
+    rm ./delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+    mv ./delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu/delta "${HOME}/workspace/bin"
+    rm -rf ./delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu
 fi
 
 echo "🐧️ Happy hacking!"
