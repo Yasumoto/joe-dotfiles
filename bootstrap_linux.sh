b7a0cab482bf847c2c26dd15329c985455a6ccec
Joe Smith <yasumoto7@gmail.com>
2022-07-17 09:03:41 -0700

    Installing flatpaks

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -94,4 +94,8 @@ if [ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]; 
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
+mkdir -p "${HOME}/workspace/github.com"
+
+fish -c "clone https://github.com/arcticicestudio/nord-gnome-terminal.git"
+
 echo "🐧 All set!"
---
+++ install_flatpaks.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+flatpak install -y flathub re.sonny.Junction
+
+flatpak install flathub de.haeckerfelix.Shortwave
+
+flatpak install -y flathub de.haeckerfelix.Fragments
+
+flatpak install -y flathub org.gabmus.gfeeds
+
+# https://flathub.org/apps/details/org.kde.kdenlive
+flatpak install -y flathub org.kde.kdenlive
+
+flatpak install -y flathub com.github.johnfactotum.Foliate
+
+flatpak install -y flathub de.gunibert.Hackgregator
+
+flatpak install -y flathub org.gabmus.hydrapaper
+
+flatpak install -y flathub org.gnome.Solanum
+
+flatpak install -y flathub org.gnome.PasswordSafe
+
+flatpak install -y flathub org.gnome.Builder
+
+flatpak install -y flathub org.gabmus.whatip
+
+flatpak install -y flathub org.kde.digikam
+
+flatpak install -y flathub org.gnome.Lollypop
+
+flatpak install -y flathub com.rafaelmardojai.Blanket
+
+flatpak install -y flathub com.github.maoschanz.drawing
+
+flatpak install -y flathub net.daase.journable
+
+flatpak install -y flathub com.gitlab.newsflash
+
+flatpak install -y flathub com.github.tchx84.Flatseal
+
+flatpak install -y flathub com.uploadedlobster.peek
+
+flatpak install -y flathub org.gnome.Connections
+
+flatpak install -y flathub com.discordapp.Discord
+
+flatpak install -y flathub com.valvesoftware.Steam
+
+flatpak install -y flathub org.videolan.VLC
+
+flatpak install -y flathub org.gimp.GIMP
+
+flatpak install -y flathub com.obsproject.Studio
+
+flatpak install -y flathub com.slack.Slack
+
+flatpak install -y flathub ar.xjuan.Cambalache
+
+flatpak install -y flathub org.gnome.Boxes
+
+flatpak install -y flathub org.gnome.dspy
+
+flatpak install -y flathub re.sonny.Workbench
+
+flatpak install -y flathub com.github.geigi.cozy
+
+flatpak install -y flathub org.gnome.NetworkDisplays
+
+flatpak install -y flathub com.github.bleakgrey.tootle
+
+flatpak install -y flathub org.gnome.Fractal
+
+flatpak install -y flathub com.github.babluboy.bookworm
+
+flatpak install -y flathub org.gnome.GTG
+
+flatpak install -y flathub codes.merritt.FeelingFinder
