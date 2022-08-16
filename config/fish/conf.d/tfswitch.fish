4b03fa3e189c8e007bb3138663231783490457a7
Joe Smith <yasumoto7@gmail.com>
2022-08-16 16:51:38 -0700

    tfswitch should work with zoxide and use workspace/bin

--- config/fish/conf.d/tfswitch.fish
+++ config/fish/conf.d/tfswitch.fish
@@ -6,14 +6,17 @@ function switch_terraform --on-event fish_postexec
     string match --regex '^cd\s' "$argv" > /dev/null
     set --local is_command_cd $status
 
-    if test $is_command_cd -eq 0
+    string match --regex '^z\s' "$argv" > /dev/null
+    set --local is_command_zoxide $status
+
+    if test $is_command_cd -eq 0 || test $is_command_zoxide -eq 0
       if count *.tf > /dev/null
 
         grep -c "required_version" *.tf > /dev/null
         set --local tf_contains_version $status
 
         if test $tf_contains_version -eq 0
-            command tfswitch
+            command tfswitch -b ~/workspace/bin/terraform
         end
       end
     end
