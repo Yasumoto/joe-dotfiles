cc91bea5ff8b1095c2834462910b01a538568375
Joe Smith <yasumoto7@gmail.com>
2023-08-25 09:17:23 -0400

    python3 & pyenv

--- home.nix
+++ home.nix
@@ -29,6 +29,7 @@
     pkgs.starship
     pkgs.vim
     pkgs.git-lfs
+    pkgs.pyenv
 
     pkgs.awscli2
     pkgs.eksctl
@@ -245,8 +246,8 @@
       set AWS_ACCESS_KEY_ID (echo $SUBSTRATE_OUTPUT | cut -f3 -d" " | cut -f2 -d\" )
       set AWS_SECRET_ACCESS_KEY (echo $SUBSTRATE_OUTPUT | cut -f4 -d" " | cut -f2 -d\" )
       set AWS_SESSION_TOKEN (echo $SUBSTRATE_OUTPUT | cut -f5 -d" " | cut -f2 -d\" )
-      python -c "from os import path; from configparser import ConfigParser; config = ConfigParser(); config.read(path.expanduser('~/.aws/credentials'))
-    if '$AWS_ACCESS_KEY_ID' == "":
+      python3 -c "from os import path; from configparser import ConfigParser; config = ConfigParser(); config.read(path.expanduser('~/.aws/credentials'))
+    if '$AWS_ACCESS_KEY_ID' == \"\":
       print('Existing creds should already be wired in!')
     else:
       for profile_name in ['default', 'refreshment_substrate']:
