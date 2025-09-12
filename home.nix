b2f3540097ff0b236ce01ec37d830fbfbf510566
Joe Smith <yasumoto7@gmail.com>
2025-09-12 08:51:24 -0700

    telescope keybindings

--- home.nix
+++ home.nix
@@ -466,6 +466,15 @@
       },
     })
 
+    vim.g.mapleader = ','
+
+    -- Telescope keybindings
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 
     require'nvim-treesitter.configs'.setup {
       -- Modules and its options go here
